b6613ae60a0b3eb52519c70140bfb6c53c019234
Vishnutha <Vishnutha.Sheela@iiitb.org>
2023-12-13 00:27:12 +0530

    config.js.vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host : true,
-    port: 5173,
     proxy: {
       "/api": {
-
-        target: "http://0.0.0.0:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       }
