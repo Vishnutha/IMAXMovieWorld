80eb8b6ee7fc86f983614a8cb3e766416a04d0b8
Vishnutha <Vishnutha.Sheela@iiitb.org>
2023-12-12 21:32:57 +0530

    config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,9 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-
         target: "http://localhost:5000",
-        changeOrigin: true,
         secure: false,
       }
     },
